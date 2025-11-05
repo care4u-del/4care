afd63f36e8dd9d00b03dfaa0acc77c78888bd9c0
Phanendra Sana <phanendraofficial@gmail.com>
2025-11-05 15:08:52 +0530

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "daily-tracker-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./favicon.png",
+  "./manifest.json"
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("💖 Caching app shell");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((res) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, res.clone());
+            return res;
+          });
+        })
+      );
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log("🧹 Clearing old cache:", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
